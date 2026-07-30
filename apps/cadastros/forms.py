f67131f50913caeca69214e71c3803e2e9490ecf
Luis Paulo <luispaulosantos.br@gmail.com>
2026-07-30 19:26:43 -0300

    Sprint 3: cadastro e listagem de duncionários

--- apps/cadastros/forms.py
+++ apps/cadastros/forms.py
@@ -7,3 +7,29 @@ class FuncionarioForm(forms.ModelForm):
     class Meta:
         model = Funcionario
         fields = "__all__"
+
+        widgets = {
+            "nome": forms.TextInput(attrs={"class": "form-control"}),
+            "cpf": forms.TextInput(attrs={"class": "form-control"}),
+            "numero_cnh": forms.TextInput(attrs={"class": "form-control"}),
+            "validade_cnh": forms.DateInput(
+                attrs={
+                    "class": "form-control",
+                    "type": "date",
+                }
+            ),
+            "funcao": forms.Select(attrs={"class": "form-select"}),
+            "cursos": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "rows": 4,
+                }
+            ),
+            "observacao": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "rows": 4,
+                }
+            ),
+            "ativo": forms.CheckboxInput(attrs={"class": "form-check-input"}),
+        }
